e47876eae4ba0d76e8b47050af25fbddf2e962f7
Nathan Gibbs <nathan@cmpublishers.com>
2014-12-01 22:43:36 +0000

    General Changes
    In Help:
    -Bumped Version
    -Updated Author Information.
    Code:
    -Removed print in syslog_input
    -Output optimizations.
    Changed cron script instructions.

--- ipvsrate.sh
+++ ipvsrate.sh
@@ -2,8 +2,8 @@
 # Copyright (C) 2009-2014 Nathan Gibbs <nathan@cmpublishers.com>
 # A Script that reports the IPVS counters via email and then resets them.
 
-# Intended to be run daily by cron.
-# Be sure to adjust the reporting email address.
+# Intended to be run by cron.
+# Be sure to change the reporting email address.
 
 ipvsadm -ln --sort --stats|egrep "^(TC|UD)P"|mail -s "Daily Flow Report" noc@example.com
 ipvsadm -Z
