f15c80375d346be34ff5de1ddb0dcb5f14dd2887
Nathan Gibbs <nathan@cmpublishers.com>
2019-02-07 21:44:08 +0000

    Install script overhaul. :smile: Minor logging output fix. Changed some Copyright dates. Changed bug reporting endpoint to Github Repo Issue Tracker. https://github.com/lennycartier/iptoip/issues

--- install.sh
+++ install.sh
@@ -1,14 +1,40 @@
 #! /bin/sh
 
+# Names & locations.
+Pkg="iptoip"
+Isl="/usr/local"
+Ism=$Isl/man
+Iss=$Isl/share
+Isd=$Iss/doc/$Pkg
+Isp=$Iss/$Pkg
+
+echo "Installing $Pkg"
 # Install Code.
-cp iptoip /usr/local/sbin
-cp iptoip.init /etc/init.d/iptoip
+cp $Pkg $Isl/sbin
+cp $Pkg.init /etc/init.d/$Pkg
 # Install Documentation.
-cp iptoip.8 /usr/local/man/man8
-cp iptoip.xml.5 /usr/local/man/man5
-mkdir -p /usr/local/share/iptoip
-cp iptoip.xml install.sh ipvsrate.sh /usr/local/share/iptoip
-mkdir -p /usr/local/share/doc/iptoip
-cp AUTHORS CHANGELOG COPYING LICENSE README README.md TODO /usr/local/share/doc/iptoip
+cp $Pkg.8 $Ism/man8
+cp $Pkg.xml.5 $Ism/man5
+mkdir -p $Isd
+cp AUTHORS CHANGELOG COPYING LICENSE README README.md TODO $Isd
+# Misc Files
+mkdir -p $Isp
+cp $Pkg.xml install.sh ipvsrate.sh $Isp
+echo "Install Complete."
+echo "Setting up $Pkg service."
 # Setup Runtime.
-chkconfig -a iptoip
+echo -n "Trying chkconfig: "
+if which chkconfig; then
+	echo -n "Found: "
+	chkconfig -a $Pkg
+	echo "Success"
+elif which update-rc.d; then
+	echo "Failed"
+	echo -n "Trying update-rc.d: "
+	update-rc.d $Pkg defaults
+	echo "Success"
+else
+	echo "Unable to setup $Pkg service."
+	echo "Pleae submit issue at:"
+	echo "https://github.com/lennycartier/iptoip/issues"
+fi
